2c21e23666209d45f9fcae70620d213f19360428
Diana Nakagawa <drcnakagawa@gmail.com>
2023-05-28 00:13:28 -0400

    added sorting through subdirectories functionality

--- app.py
+++ app.py
@@ -18,8 +18,8 @@ def get_image_date(image_path):
     return None, None
 
 # Source and destination directories
-source_directory = "/home/alohawot/Desktop/2020/202001_a"
-destination_directory = "/home/alohawot/Desktop/sorted"
+source_directory = "/your/source/directory"
+destination_directory = "/your/destination/directory"
 
 # Create destination directory if it doesn't exist
 if not os.path.exists(destination_directory):
@@ -30,36 +30,37 @@ num_sorted = 0
 num_not_sorted = 0
 formats_not_sorted = set()
 
-# Iterate over files in the source directory
-for filename in os.listdir(source_directory):
-    file_path = os.path.join(source_directory, filename)
+# Traverse through all subdirectories in the source directory
+for root, dirs, files in os.walk(source_directory):
+    for filename in files:
+        file_path = os.path.join(root, filename)
 
-    # Check if the file is an image
-    if os.path.isfile(file_path) and filename.lower().endswith((".jpg", ".jpeg", ".png")):
-        # Get the year and month when the image was taken
-        year, month = get_image_date(file_path)
+        # Check if the file is an image
+        if filename.lower().endswith((".jpg", ".jpeg", ".png")):
+            # Get the year and month when the image was taken
+            year, month = get_image_date(file_path)
 
-        if year is not None and month is not None:
-            # Create a directory for the year if it doesn't exist
-            year_directory = os.path.join(destination_directory, year)
-            if not os.path.exists(year_directory):
-                os.makedirs(year_directory)
+            if year is not None and month is not None:
+                # Create a directory for the year if it doesn't exist
+                year_directory = os.path.join(destination_directory, year)
+                if not os.path.exists(year_directory):
+                    os.makedirs(year_directory)
 
-            # Create a directory for the month if it doesn't exist
-            month_directory = os.path.join(year_directory, month)
-            if not os.path.exists(month_directory):
-                os.makedirs(month_directory)
+                # Create a directory for the month if it doesn't exist
+                month_directory = os.path.join(year_directory, month)
+                if not os.path.exists(month_directory):
+                    os.makedirs(month_directory)
 
-            # Move the image to the appropriate directory
-            destination_path = os.path.join(month_directory, filename)
-            shutil.move(file_path, destination_path)
-            print(f"Moved {filename} to {month_directory}")
-            num_sorted += 1
-        else:
-            print(f"Could not determine the year for {filename}")
-            num_not_sorted += 1
-            file_format = os.path.splitext(filename)[1]
-            formats_not_sorted.add(file_format)
+                # Move the image to the appropriate directory
+                destination_path = os.path.join(month_directory, filename)
+                shutil.move(file_path, destination_path)
+                print(f"Moved {filename} to {month_directory}")
+                num_sorted += 1
+            else:
+                print(f"Could not determine the year for {filename}")
+                num_not_sorted += 1
+                file_format = os.path.splitext(filename)[1]
+                formats_not_sorted.add(file_format)
 
 print("Sorting completed.")
 print(f"Files sorted: {num_sorted}")
